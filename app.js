6672f14129aef24d854eb443c1172e45d86619ca
Atul Varma <varmaa@gmail.com>
2014-11-11 02:58:55 -0500

    use dinky password

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 
 var PORT = process.env.PORT || 3000;
+var PASSWORD = process.env.PASSWORD;
 
 var app = express();
+
+function checkPassword(req, res, next) {
+  if (req.headers['x-dinky-password'] != PASSWORD)
+    return res.sendStatus(403);
+  next();
+}
+
+app.use('/api/', checkPassword);
+
+app.get('/api/check', function(req, res, next) {
+  res.send({ok: true});
+});
 
 app.use(express.static(__dirname + '/static'));
 
--- static/main.jsx
+++ static/main.jsx
@@ -3,27 +3,24 @@
     mixins: [React.addons.LinkedStateMixin],
     getInitialState: function() {
       return {
-        email: '',
         password: ''
       };
     },
     handleSubmit: function(e) {
       e.preventDefault();
-      this.props.firebaseRef.authWithPassword({
-        email: this.state.email,
+      this.props.serverAPI.auth({
         password: this.state.password
-      }, function(error, authData) {
+      }, function(error) {
         if (error) {
           alert("Invalid credentials!");
         } else {
-          this.props.onLogin(authData);
+          this.props.onLogin();
         }
       }.bind(this));
     },
     render: function() {
       return (
         <form onSubmit={this.handleSubmit}>
-          <input type="text" className="form-control" valueLink={this.linkState('email')} placeholder="email"/>
           <input type="password" className="form-control" valueLink={this.linkState('password')} placeholder="password"/>
           <button>Log in to see more</button>
         </form>
@@ -41,10 +38,7 @@
     handleSubmit: function(e) {
       e.preventDefault();
       if (!this.state.text) return;
-      var auth = this.props.firebaseRef.getAuth();
-      this.props.firebaseRef.push({
-        from: auth.uid,
-        fromEmail: auth.password.email,
+      this.props.serverAPI.push({
         text: this.state.text
       });
       this.setState({text: ''});
@@ -68,38 +62,87 @@
   var App = React.createClass({
     getInitialState: function() {
       return {
-        authData: this.props.firebaseRef.getAuth()
+        isLoggedIn: this.props.serverAPI.getAuth()
       };
     },
     handleLogout: function() {
-      this.props.firebaseRef.unauth();
-      this.setState({authData: null});
+      this.props.serverAPI.unauth();
+      this.setState({isLoggedIn: false});
     },
-    handleLogin: function(authData) {
-      this.setState({authData: authData});
+    handleLogin: function() {
+      this.setState({isLoggedIn: true});
     },
     render: function() {
       var contents;
 
-      if (this.state.authData) {
+      if (this.state.isLoggedIn) {
         contents = (
           <div>
-            <CreateForm firebaseRef={this.props.firebaseRef.child('posts')}/>
-            <button style={{fontSize: 12, marginTop: 10}} onClick={this.handleLogout}>Log out {this.state.authData.password.email}</button>
+            <CreateForm serverAPI={this.props.serverAPI}/>
+            <button style={{fontSize: 12, marginTop: 10}} onClick={this.handleLogout}>Log out</button>
           </div>
         );
       } else {
-        contents = <LoginForm firebaseRef={this.props.firebaseRef} onLogin={this.handleLogin} />;
+        contents = <LoginForm serverAPI={this.props.serverAPI} onLogin={this.handleLogin} />;
       }
       return <div className="container">{contents}</div>;
     }
   });
 
-  function start() {
-    var firebaseRef = new Firebase("https://get-well-soon-jess.firebaseio.com/");
+  function ServerAPI() {
+    var self = {};
+    var password = window.localStorage['GWSJ_DINKY_PW'] || null;
 
+    function request(method, url, data, cb) {
+      if (typeof(data) == 'function') {
+        cb = data;
+        data = null;
+      }
+
+      var req = new XMLHttpRequest();
+
+      req.open(method, url);
+      req.onload = function() {
+        if (req.status == 403)
+          return cb(new Error('Invalid password'));
+        if (req.status != 200)
+          return cb(new Error('Got ' + req.status));
+        cb(null, JSON.parse(req.responseText));
+      };
+      req.onerror = function() {
+        cb(new Error('Network error'));
+      };
+      req.setRequestHeader('X-Dinky-Password', password);
+      req.send(data ? JSON.stringify(data) : null);
+    }
+
+    self.getAuth = function() {
+      return !!password;
+    };
+
+    self.auth = function(options, cb) {
+      password = options.password;
+      request('GET', '/api/check', function(err, data) {
+        if (err) {
+          self.unauth();
+          return cb(err);
+        }
+        window.localStorage['GWSJ_DINKY_PW'] = password;
+        cb(null);
+      });
+    };
+
+    self.unauth = function() {
+      delete window.localStorage['GWSJ_DINKY_PW'];
+      password = null;
+    };
+
+    return self;
+  }
+
+  function start() {
     var app = React.render(
-      <App firebaseRef={firebaseRef}/>,
+      <App serverAPI={ServerAPI()}/>,
       document.getElementById('app')
     );
 
